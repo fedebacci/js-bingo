2d7630a51ec6097ad70dac817ae59df9699854fd
fedebacci <federicobacci1999@gmail.com>
2025-04-06 10:56:30 +0200

    sistemazione struttura per accettazione piu versioni esercizio

---
+++ js-bingo-2/assets/js/main.js
@@ -0,0 +1,3 @@
+// Va implementata una partita a tombola, tra 1 giocatore e 1 pc.
+// Vanno quindi estratti a caso 15 numeri, per la cartella del giocatore e per la cartella del pc.
+// In seguito vanno estratti a caso dei numeri, fino a che uno dei due giocatori non vince la partita.
